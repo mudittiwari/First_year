1cb571bd098a01291f1cf8e4e5deb4e491f3de28
mudittiwari <68576920+mudittiwari@users.noreply.github.com>
2021-05-16 18:55:45 +0530

    Create first_session.py

---
+++ first_session.py
@@ -0,0 +1,37 @@
+# a="mudit" #string
+# b=10      #integer
+# c=10.1    #float
+# d='a'     #character
+
+# print(a,end="")
+# print(b,end=" ")
+# print(c,end=" ")
+# print(d,end=" ")
+
+# var=input("type the variable")
+# print(var)
+
+# var =int(input("type the number"))
+# print(type(var))
+
+# a=str(int(input("type first number")))
+# b=str(int(input("type second number")))
+# print(a+b)
+
+# a=True
+# print(type(10))
+
+# var="5090000"
+# print(var[2])
+
+# var="mudit"
+# print(var[0:2])
+
+# m=0
+# u=1
+# d=2
+# i=3
+# t=4
+
+# print("mudit","\t","tiwari")
+# print("tiwari")
