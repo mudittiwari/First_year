6f72d46ae87152dfb600682df7eb389476ade503
mudittiwari <68576920+mudittiwari@users.noreply.github.com>
2021-05-17 19:28:12 +0530

    Create second_session.py

---
+++ second_session.py
@@ -0,0 +1,51 @@
+var="mudit"
+# temp=var.capitalize()
+# print(var)
+# print(temp)
+
+# cnt=var.count("um")
+# print(cnt)
+
+# temp=var.endswith("mumumu")
+# print(temp)
+
+# index=var.index("m")
+# print(index)
+
+# index=var.find("x")
+# print(index)
+
+# temp=var.islower()
+# print(temp)
+
+# var=var.replace("MuD","MD")
+# print(var)
+
+# temp=var.startswith("Mu")
+# print(temp)
+
+# var=var.upper()
+# print(var)
+
+temp_list=[
+    10,5,11,12,3,5
+]
+
+# temp_list.sort()
+
+# temp_list.reverse()
+
+# temp_list.remove(12)
+
+# print(temp_list.index(5))
+
+# temp_list.insert(2,20)
+
+# print(temp_list.count(5))
+
+# temp_list.append(21)
+
+print(temp_list.pop())
+
+print(temp_list)
+
