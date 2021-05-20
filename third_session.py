f95ce6dd1d787f4b075ab1f801936a13c3ff86af
mudittiwari <68576920+mudittiwari@users.noreply.github.com>
2021-05-20 18:35:59 +0530

    Create third_session.py

---
+++ third_session.py
@@ -0,0 +1,53 @@
+# var=[1,2,3,4,5]
+# print(var[2:5])
+
+# var=(1,2,3,4,"mudit",'a')
+# print(var.index(7))
+# print(var.count(2))
+
+# var={
+#     "name":"mudit",
+#     2:"tiwari",
+# }
+# var.clear()
+# var["name"]
+# print(var.get("name"))
+# print(var.pop("name"))
+
+# print(var.values())
+# print(var.popitem())
+
+# var["name"]="mansi"
+# var["sname"]="gupta"
+
+# a=int(input("type the number"))
+# if a%2==0:
+#     print("even number")
+# elif (Condition):
+#     pass
+# else:
+#     print("odd number")
+
+# var=[1,2,3,4,5,6,7]
+
+# for i in var:
+#     print(i)
+
+# for i in range(0,7):
+#     print(var[i])
+
+# i=0
+# while i<10:
+#     print(i)
+#     i=i+1
+#     i+=1
+
+# +,-,%,/,//,**--arithmetic operators
+
+# +=,=,-= assignment operators
+
+# ==,>=,>,<,<= --comparison operators
+
+
+
+
